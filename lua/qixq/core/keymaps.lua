a1cb90e612afad420023e596f5354e24de300dd0
qixq <qi728@outlook.com>
2023-01-24 23:34:27 +0800

    feat: 增加快速移动

--- lua/qixq/core/keymaps.lua
+++ lua/qixq/core/keymaps.lua
@@ -2,13 +2,19 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
+
 -- ---------- 插入模式 ---------- ---
 keymap.set("i", "jk", "<ESC>")
 
 -- ---------- 视觉模式 ---------- ---
--- 但行或多行移动
+-- 单行或多行移动
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- ---------- 普通模式 ---------- ---
+-- 快速移动 6行
+keymap.set("n", "J", "6j")
+keymap.set("n", "K", "6k")
 
 -- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
